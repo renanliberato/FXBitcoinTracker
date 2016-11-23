8d56236722de657c80f3baedb4f08f81705093f9
Renan Liberato <renan.libsantana@gmail.com>
2016-11-23 01:00:04 -0200

    HomeController commit

---
+++ src/app/home/HomeController.java
@@ -0,0 +1,23 @@
+package app.home;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by renan on 11/22/2016.
+ */
+public class HomeController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+}
