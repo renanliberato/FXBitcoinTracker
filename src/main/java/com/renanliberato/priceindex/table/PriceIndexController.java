0102c2038a1fb0d5a7c9ae232bfaf52c51b56714
Renan Liberato <renan.libsantana@gmail.com>
2016-12-23 17:11:32 -0200

    Formatted dates showed at the table.

--- src/main/java/com/renanliberato/priceindex/table/PriceIndexController.java
+++ src/main/java/com/renanliberato/priceindex/table/PriceIndexController.java
@@ -3,6 +3,8 @@ package com.renanliberato.priceindex.table;
 import com.renanliberato.priceindex.model.PriceIndex;
 import com.renanliberato.priceindex.search.SearchController;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
@@ -11,8 +13,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 /**
@@ -34,7 +41,21 @@ public class PriceIndexController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        dateColumn.setCellValueFactory(new PropertyValueFactory<PriceIndex, String>("date"));
+        dateColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<PriceIndex, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<PriceIndex, String> index) {
+                SimpleStringProperty property = new SimpleStringProperty();
+                DateFormat fromDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                DateFormat toDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                try {
+                    property.setValue(toDateFormat.format(fromDateFormat.parse(index.getValue().getDate())));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                return property;
+            }
+        });
         indexColumn.setCellValueFactory(new PropertyValueFactory<PriceIndex, Double>("index"));
 
         searchController.getSearchButton().setOnAction((ActionEvent event) -> {
